e517dd0975e9c4b2fbf2b86ab3dd772e6d1a43a6
GabrielPrad <89950741+GabrielPrad@users.noreply.github.com>
2023-05-08 14:36:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 function calcular() {
-    var n1 = parseFloat(document.getElementById("n1").value);
-    var n2 = parseFloat(document.getElementById("n2").value);
-    var n3 = parseFloat(document.getElementById("n3").value);
+  var n1 = parseFloat(document.getElementById("n1").value);
+  var n2 = parseFloat(document.getElementById("n2").value);
+  var n3 = parseFloat(document.getElementById("n3").value);
 
-    // verifica se as notas estão dentro do intervalo válido
-    if (n1 < 0 || n1 > 100 || n2 < 0 || n2 > 100 || n3 < 0 || n3 > 100) {
-        alert("As notas informadas devem estar entre 0 e 100.");
-        return;
-    }
+  if (n1 < 0 || n1 > 100 || n2 < 0 || n2 > 100 || n3 < 0 || n3 > 100) {
+    alert("As notas informadas devem estar entre 0 e 100.");
+    return;
+  }
 
-    var media = (n1 + n2 + n3) / 3;
+  var media = (n1 + n2 + n3) / 3;
 
-    alert(`A média dos valores é: ${media.toFixed(2)}`);
+  var inputMedia = document.getElementById("media");
+  inputMedia.value = media.toFixed(2);
+
+  if (media >= 70) {
+    inputMedia.classList.add("verde");
+    inputMedia.classList.remove("amarelo");
+    inputMedia.classList.remove("vermelho");
+  } else if (media >= 40) {
+    inputMedia.classList.add("amarelo");
+    inputMedia.classList.remove("verde");
+    inputMedia.classList.remove("vermelho");
+  } else {
+    inputMedia.classList.add("vermelho");
+    inputMedia.classList.remove("verde");
+    inputMedia.classList.remove("amarelo");
+  }
+
+  if (media >= 70) {
+    alert("Você foi aprovado!");
+  } else if (media >= 40) {
+    alert("Exame final");
+  } else {
+    alert("Reprovado!");
+  } 
+  return;
 }
 
-function verificarMedia(media) {
-    if (media => 70) {
-      createMessage('Aprovado!', 'sucess');
-    } else {
-        createMessage('Reprovado!', 'warning')
-    }
-}
+  if (media >= 70) {
+    alert("Você foi aprovado!")
+  } else if (media >= 40) {
+    alert("Você ficou de recuperação")
+  } else {
+    inputMedia.classList.add("vermelho");
+    inputMedia.classList.remove("verde");
+    inputMedia.classList.remove("amarelo");
+  } 
 
-function createMessage(msg, type) {
-    document
-      .querySelector('body')
-      .insertAdjacentHTML(
-        'beforebegin',
-        `<div class='message ${type}'>${msg}</div>`
-      );
-  
-    setTimeout(function () {
-      deleteMessage();
-    }, 3000);
-  }
-  
-  function deleteMessage() {
-    const list = document.querySelectorAll('.message');
-    for (const item of list) {
-      item.remove();
-    }
-  }
-    
-    
+
